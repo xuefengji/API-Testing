7d47c13b9575360e215c0799eb5b29b779df0c5f
xuefengji <jxfvip@sina.com>
2019-11-29 17:50:19 +0800

    feat:返回json

--- Imooc/web/views.py
+++ Imooc/web/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.shortcuts import render
+import json
 
 # Create your views here.
 
 def Login(request):
     if request.method == 'POST':
+        result = {}
         user = request.POST.get('username')
-        return HttpResponse(user)
+        password = request.POST.get('password')
+        result['user'] = user
+        result['password'] = password
+        result = json.dumps(result)
+        return HttpResponse(result,content_type='application/json;charset=utf-8')
     else:
         return render(request,'login.html')
