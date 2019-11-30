7821fec2aa2f63ae21c0366a2f9c857f81413e59
xuefengji <jxfvip@sina.com>
2019-11-30 20:13:32 +0800

    feat:使用类封装

--- post.py
+++ post.py
@@ -1,22 +1,29 @@
 import requests
+import json
 
+class Run_Main():
+    def __init__(self,url,data,method):
+       self.res = self.run(url,data,'POST')
 
-def send_post(url,data):
-    res = requests.post(url,data)
-    return res.json()
-def send_get(url,data):
-    res = requests.get(url,data)
-    return res.json()
-def run(url,data,method):
-    res = None
-    if method == 'POST':
-        res = send_post(url,data)
-    if method == 'GET':
-        res = send_get(url,data)
-    return res
+    def send_post(self,url,data):
+        res = requests.post(url,data)
+        return res.json()
+
+    def send_get(self,url,data):
+        res = requests.get(url,data)
+        return res.json()
+
+    def run(self,url,data,method):
+        res = None
+        if method == 'POST':
+            res = self.send_post(url,data)
+        if method == 'GET':
+            res = self.send_get(url,data)
+        return res
 
 if __name__=='__main__':
     url = 'http://127.0.0.1:8000/login/'
     data = {'username': 'dddddd', 'password': 11111111}
-    res = run(url,data,"POST")
-    print(res)
+    res = Run_Main(url,data,'POST')
+
+    print(res.res)
