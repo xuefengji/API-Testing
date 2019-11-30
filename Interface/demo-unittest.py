cafc7d2fdbedae66dbbd9db961fe12ce64a7f998
xuefengji <jxfvip@sina.com>
2019-11-30 21:19:25 +0800

    feat:增加unittestdemo

--- Interface/demo-unittest.py
+++ Interface/demo-unittest.py
@@ -1,6 +1,19 @@
 import unittest
 
 class demo(unittest.TestCase):
-    
+    #每次方法前调用
     def setUp(self):
-        print()
+        print('每次方法前调用')
+
+    def tearDown(self):
+        print('每次方法后调用')
+
+    def test_01(self):
+        print('这是第一个方法')
+
+    def test_02(self):
+        print('这是第二个方法')
+
+
+if __name__=='__main__':
+    unittest.main()
