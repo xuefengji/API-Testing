f926020ad07952e3d05f50701d7f23cee4c0255e
xuefengji <jxfvip@sina.com>
2019-11-30 19:43:42 +0800

    feat:使用函数封装

--- post.py
+++ post.py
@@ -1,9 +1,22 @@
 import requests
 
 
-url = 'http://127.0.0.1:8000/login/'
+def send_post(url,data):
+    res = requests.post(url,data)
+    return res.json()
+def send_get(url,data):
+    res = requests.get(url,data)
+    return res.json()
+def run(url,data,method):
+    res = None
+    if method == 'POST':
+        res = send_post(url,data)
+    if method == 'GET':
+        res = send_get(url,data)
+    return res
 
-data = {'username':'dddddd','password':11111111}
-
-res = requests.get(url,params=data)
-print(res.json())
+if __name__=='__main__':
+    url = 'http://127.0.0.1:8000/login/'
+    data = {'username': 'dddddd', 'password': 11111111}
+    res = run(url,data,"POST")
+    print(res)
