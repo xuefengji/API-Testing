34c2341aef6e829e25f88c5d8e905e23f3e26468
xuefengji <jxfvip@sina.com>
2019-12-01 15:13:04 +0800

    feat:unittest 跳过case的功能

--- Interface/caseTest.py
+++ Interface/caseTest.py
@@ -12,14 +12,19 @@ class TestMethod(unittest.TestCase):
         self.assertEqual(data['password'], 1111111, '测试失败')
 
         print(res)
-
+    @unittest.skip
     def test_02(self):
         data = {'username': 'wewewee', 'password': 2222222}
         res = self.run.run_main(self.url, data, 'GET')
         #添加断言，'测试失败'：不相等的时候的提示的msg
-        self.assertEqual(data['password'], 1111111, '测试失败')
+        self.assertEqual(data['password'], 2222222, '测试失败')
         print(res)
 
 
 if __name__=='__main__':
+    #使用unittest中的main函数执行case:
     unittest.main()
+    #使用unittest的TestSuite执行case：
+    # suite = unittest.TestSuite()
+    # suite.addTest(TestMethod('test_01'))
+    # unittest.TextTestRunner.run(suite)
