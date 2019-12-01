4df0cb112d8ab8126c101dd4bfb871b707e5ac27
xuefengji <jxfvip@sina.com>
2019-12-01 15:00:41 +0800

    feat:增加断言assert

--- Interface/caseTest.py
+++ Interface/caseTest.py
@@ -8,11 +8,16 @@ class TestMethod(unittest.TestCase):
     def test_01(self):
         data = {'username': 'sdsdsdsd', 'password': 1111111}
         res = self.run.run_main(self.url,data,'POST')
+        #添加断言
+        self.assertEqual(data['password'], 1111111, '测试失败')
+
         print(res)
 
     def test_02(self):
         data = {'username': 'wewewee', 'password': 2222222}
         res = self.run.run_main(self.url, data, 'GET')
+        #添加断言，'测试失败'：不相等的时候的提示的msg
+        self.assertEqual(data['password'], 1111111, '测试失败')
         print(res)
 
 
