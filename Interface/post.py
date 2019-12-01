0a5b1f96f22215ade00d7009de29a948b90658a5
xuefengji <jxfvip@sina.com>
2019-12-01 10:42:20 +0800

    feat:修改post功能

--- Interface/post.py
+++ Interface/post.py
@@ -2,8 +2,9 @@ import requests
 import json
 
 class Run_Main():
-    def __init__(self,url,data,method):
-       self.res = self.run(url,data,'POST')
+    #初始化
+    # def __init__(self,url,data,method):
+    #    self.res = self.run(url,data,'POST')
 
     def send_post(self,url,data):
         res = requests.post(url,data)
@@ -13,7 +14,7 @@ class Run_Main():
         res = requests.get(url,data)
         return res.json()
 
-    def run(self,url,data,method):
+    def run_main(self,url,data,method):
         res = None
         if method == 'POST':
             res = self.send_post(url,data)
@@ -24,6 +25,7 @@ class Run_Main():
 if __name__=='__main__':
     url = 'http://127.0.0.1:8000/login/'
     data = {'username': 'dddddd', 'password': 11111111}
-    res = Run_Main(url,data,'POST')
-
-    print(res.res)
+    # res = Run_Main(url,data,'POST')
+    run = Run_Main()
+    res = run.run_main(url,data,'POST')
+    print(res)
