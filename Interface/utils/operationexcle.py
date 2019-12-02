e2b24f09b11a18d198beb491f4db699a0356f359
xuefengji <jxfvip@sina.com>
2019-12-02 14:28:06 +0800

    feat:封装操作excel功能

--- Interface/utils/operationexcle.py
+++ Interface/utils/operationexcle.py
@@ -1,8 +1,34 @@
 import xlrd
 
-exlce_data = xlrd.open_workbook('../datas/excel_data.xlsx')
+class OperationExcel:
 
-sheet = exlce_data.sheet_by_index(0)
+    def __init__(self,file_name=None,sheet_id=None):
+        if file_name:
+            self.file_name = file_name
+            self.sheet_id = sheet_id
+        else:
+            #设置默认路径
+            self.file_name = '../datas/excel_data.xlsx'
+            self.sheet_id = 0
+        self.data = self.get_data()
 
-print(sheet.cell(1,3))
-print(sheet.nrows)
+    def get_data(self):
+        exlce_data = xlrd.open_workbook(self.file_name)
+        tables = exlce_data.sheet_by_index(self.sheet_id)
+        return tables
+
+    #得到表格行数
+    def get_rows(self):
+        rows = self.data.nrows
+        return rows
+
+    #得到具体单元格数据
+    def get_cell_value(self,row,col):
+        data = self.data.cell(row,col)
+        return data
+
+if __name__=='__main__':
+    tables = OperationExcel()
+
+    print(tables.get_cell_value(1,3))
+
