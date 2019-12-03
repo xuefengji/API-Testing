26b9c9df595c3099f18b8e06a4f9cdd795bd6b17
xuefengji <jxfvip@sina.com>
2019-12-03 16:49:50 +0800

    refactor:修改post和get方法

--- Interface/base/run_method.py
+++ Interface/base/run_method.py
@@ -2,26 +2,26 @@ import requests
 
 class RunMethod:
     #封装post请求
-    def post_main(self,url,data,header=None):
-        res = None
-        if header != None:
-            res = requests.post(url,data,headers=header).json()
-        else:
-            res = requests.post(url,data).json()
-        return res
+    def post_main(self,url,data):
+        # res = None
+        # if header != None:
+        #     res = requests.post(url=url,data=data,headers=header)
+        # else:
+        res = requests.post(url,data)
+        return res.json()
     #封装get请求
-    def get_main(self,url,data=None,header=None):
-        res = None
-        if header != None:
-            res = requests.get(url, data=data, headers=header).json()
-        else:
-            res = requests.get(url, data=data).json()
-        return res
+    def get_main(self,url,data=None):
+        # res = None
+        # if header != None:
+        #     res = requests.get(url=url, data=data, headers=header)
+        # else:
+        res = requests.get(url, data)
+        return res.json()
     #使用主函数控制请求的发送
-    def run_main(self,method,url,data,header):
+    def run_main(self,method,url,data):
         res = None
         if method == 'post':
-            res = self.post_main(url,data,header)
+            res = self.post_main(url,data)
         else:
-            res = self.get_main(url,data,header)
+            res = self.get_main(url,data)
         return res
