f7b8eb2e1ee71de42c7344e1f587d906bbaafd2f
xuefengji <jxfvip@sina.com>
2019-12-06 14:58:11 +0800

    feat:增加对https的请求处理

--- Interface/base/run_method.py
+++ Interface/base/run_method.py
@@ -4,8 +4,8 @@ import json
 class RunMethod:
     #封装post请求
     def post_main(self,url,data,cookie):
-
-        res = requests.post(url,data,cookies=cookie)
+        #verify=false 关闭验证，允许https的请求
+        res = requests.post(url,data,cookies=cookie,verify=False)
         return res.json()
     #封装get请求
     def get_main(self,url,data=None):
@@ -13,7 +13,7 @@ class RunMethod:
         # if header != None:
         #     res = requests.get(url=url, data=data, headers=header)
         # else:
-        res = requests.get(url, data)
+        res = requests.get(url, data,verify=False)
         return res.json()
     #使用主函数控制请求的发送
     def run_main(self,method,url,data,cookie=None):
