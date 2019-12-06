3f48d67b42f220691384ad1e8f7cdc29abe71bec
xuefengji <jxfvip@sina.com>
2019-12-06 13:46:22 +0800

    feat:增加操作数据库功能

---
+++ Interface/util/operation_mysql.py
@@ -0,0 +1,25 @@
+import pymysql
+
+class OperationMysql:
+    #初始化时连接数据库
+    def __init__(self):
+        self.db = pymysql.connect(
+        host="localhost",
+        port=3306,
+        db='',
+        user='',
+        password='',
+        charset='utf8')
+        self.cur = self.db.cursor()
+
+    #获取数据
+    def get_sql_one_data(self,sql):
+        try:
+            self.cur.execute(sql)
+            data = self.cur.fetchone()
+            return data
+        except Exception as e:
+            print(e)
+
+
+
